70a32cc431ba89cc9dd758739aab463239945065
SavuGeorgiana20 <savumaria23@stud.ase.ro>
2024-10-23 16:09:14 +0300

    Add files via upload

---
+++ seminar3.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+
+using namespace std;
+
+class Cofetarie
+{
+public:
+	string nume;
+	int nrAngajati;
+	bool esteVegana;
+	const int anDeschidere;
+	float adaos;
+	static int TVA;
+
+	Cofetarie() :adaos(30), anDeschidere(2024) 
+	{
+		this->nume = "Delice";
+		this->nrAngajati = 3;
+		this->esteVegana = true;
+		
+	}
+	
+	Cofetarie(string nume, int nrAngajati, bool esteVegana, int an, float adaos) :anDeschidere(an)
+	{
+		this->nume = nume;
+		this->nrAngajati = nrAngajati;
+		this->esteVegana = esteVegana;
+		this->adaos = adaos;
+
+	}
+	void afisare()
+	{
+		cout << "Nume: " << this->nume << endl;
+		cout << "Nr. angajati: " << this->nrAngajati << endl;
+		cout << "E vegana?: " << (this->esteVegana ? "da" : "nu") << endl;
+		cout << "An deschidere: " << this->anDeschidere << endl;
+		cout << "Adaos: " << this->adaos << endl;
+		cout << "TVA: " << this->TVA << endl;
+	}
+	
+	static void modificaTVA(int noulTVA)
+	{
+		if (noulTVA > 0)
+			Cofetarie::TVA = noulTVA;
+		
+	}
+	
+	static int calculareNrTotalAngajati(int nrCofetarii, Cofetarie* vector)
+	{
+		int suma = 0;
+		for (int i = 0; i < nrCofetarii; i++)
+			suma += vector[i].nrAngajati;
+		return suma;
+	}
+};
+int Cofetarie::TVA = 9;
+
+
+int main()
+{
+	Cofetarie c1;
+	c1.afisare();
+	Cofetarie::modificaTVA(-19);
+	Cofetarie c2("DolceVita", 6, true, 2019, 10);
+	c2.afisare();
+
+	Cofetarie* pointer;
+	pointer = new Cofetarie("Ana State", 3, false, 2020, 20);
+	
+	pointer->afisare();
+	
+	delete pointer;
+
+	int nrCofetarii = 3;
+	Cofetarie* vector;
+	vector = new Cofetarie[nrCofetarii];
+	
+	for (int i = 0; i < nrCofetarii; i++)
+	{
+		
+		vector[i].afisare();
+	}
+	cout << "Nr total angajati: " << Cofetarie::calculareNrTotalAngajati(nrCofetarii, vector);
+	delete[]vector;
+
+}
